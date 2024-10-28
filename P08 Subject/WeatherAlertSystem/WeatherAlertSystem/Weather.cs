194c7c6e78d6257525a775312161fb138ab6f995
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2024-10-28 13:27:02 +0100

    Weather Alert System

---
+++ P08 Subject/WeatherAlertSystem/WeatherAlertSystem/Weather.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace WeatherAlertSystem
+{
+    public class Weather
+    {
+        public string? City { get; set; }
+        public float Temperature { get; set; }
+        public int Precipitation { get; set; }
+    }
+}
+
