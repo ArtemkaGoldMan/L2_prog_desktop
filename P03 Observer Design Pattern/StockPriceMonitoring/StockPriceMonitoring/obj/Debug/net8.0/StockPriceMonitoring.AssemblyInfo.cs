958ccdf532b08a66e4ade6ebeb4d09e858209c43
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2024-11-11 14:48:42 +0100

    resolve iss

--- P03 Observer Design Pattern/StockPriceMonitoring/StockPriceMonitoring/obj/Debug/net8.0/StockPriceMonitoring.AssemblyInfo.cs
+++ P03 Observer Design Pattern/StockPriceMonitoring/StockPriceMonitoring/obj/Debug/net8.0/StockPriceMonitoring.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockPriceMonitoring")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f4a91290f31ef0e59ccacb9ce411553873f6db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad57d987728678ebd18b9a9546317ceb26a3d4ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockPriceMonitoring")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockPriceMonitoring")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
